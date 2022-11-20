b3ff7023406ea1424811fc27b8bb81b6bfbfa06d
nazrulbspi5 <nazrulbspi5@gmail.com>
2022-11-21 00:15:07 +0600

    Create Web Project

---
+++ src/Dreams/Dreams.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dreams.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
